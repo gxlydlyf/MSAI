2b705cbc9635fcc613331e3dbdd132104a02189c
gxlydlyf <x1k1l1@163.com>
2024-01-13 15:01:37 +0800

    update.

--- unpack.bat
+++ unpack.bat
@@ -1,6 +1,5 @@
 pyinstaller -F --version-file "file_version_info.txt" --upx-dir ".\upx" -i "icons8-minecraft-512.ico" -n "Minecraft" --clean "main.py"
 del /S /Q Minecraft.spec
 rmdir /S /Q build
-rmdir /S /Q test
-mkdir test
+del /S /Q .\test\*
 copy .\dist\Minecraft.exe .\test\Minecraft.exe
